0a8788739673b3ccfa5a8e213feff92fab48cf40
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-05-26 16:05:12 +0500

    Lucky Numbers

---
+++ lucky-numbers.js
@@ -0,0 +1,35 @@
+// @ts-check
+
+/**
+ * Calculates the sum of the two input arrays.
+ *
+ * @param {number[]} array1
+ * @param {number[]} array2
+ * @returns {number} sum of the two arrays
+ */
+export function twoSum(array1, array2) {
+  return eval(+array1.join("") + +array2.join(""));
+}
+
+/**
+ * Checks whether a number is a palindrome.
+ *
+ * @param {number} value
+ * @returns {boolean} whether the number is a palindrome or not
+ */
+export function luckyNumber(value) {
+  return [...`${value}`].reverse().join("") === `${value}`;
+}
+
+/**
+ * Determines the error message that should be shown to the user
+ * for the given input value.
+ *
+ * @param {string|null|undefined} input
+ * @returns {string} error message
+ */
+export function errorMessage(input) {
+  if (Number(input)) return "";
+  else if (!Boolean(input)) return "Required field";
+  else return "Must be a number besides 0";
+}
