7e01aa9cc7850a03dff60d10f7f881ee4230544b
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-05-27 08:16:10 +0500

    Elyses Destructured Enchantments

---
+++ enchantments2.js
@@ -0,0 +1,68 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Get the first card in the given deck
+ *
+ * @param {Card[]} deck
+ *
+ * @returns {Card} the first card in the deck
+ */
+export function getFirstCard(deck) {
+  return deck.shift();
+}
+
+/**
+ * Get the second card in the given deck
+ *
+ * @param {Card[]} deck
+ *
+ * @returns {Card} the second card in the deck
+ */
+export function getSecondCard(deck) {
+  return deck[1];
+}
+
+/**
+ * Switch the position of the first two cards in the given deck
+ *
+ * @param {Card[]} deck
+ *
+ * @returns {Card[]} new deck with reordered cards
+ */
+export function swapTopTwoCards(deck) {
+  deck.unshift(...deck.splice(1, 1));
+  return deck;
+}
+
+/**
+ * Put the top card of the given deck into a separate discard pile
+ *
+ * @param {Card[]} deck
+ *
+ * @returns {[Card, Card[]]} the top card of the given
+ * deck and a new deck containing all the other cards
+ */
+export function discardTopCard(deck) {
+  const [first, ...rest] = deck;
+  return [first, rest];
+}
+
+/** @type Card[] **/
+const FACE_CARDS = ["jack", "queen", "king"];
+
+/**
+ * Insert face cards into the given deck
+ *
+ * @param {Card[]} deck
+ *
+ * @returns {Card[]} new deck where the second,
+ * third, and fourth cards are the face cards
+ */
+export function insertFaceCards(deck) {
+  if (deck.length !== 0) {
+    deck.splice(1, 0, "jack", "queen", "king");
+    return deck;
+  }
+  return [undefined, "jack", "queen", "king"];
+}
